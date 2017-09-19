659bf378aa69f401335f06e3b36dc3ab064d6400
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-18 19:16:28 -0500

    fix adding rows to table

--- src/com/TheIronYard/CombinedArms.java
+++ src/com/TheIronYard/CombinedArms.java
@@ -1,4 +1,17 @@
 package com.TheIronYard;
 
 public interface CombinedArms extends OffensiveDoctrine {
+
+  Bomb bomber();
+
+
+
+
+  class Bomb{
+
+      @Override
+      public String toString() {
+          System.out.println("Incoming!");
+      }
+  }
 }
